b47203452004cc132967eda8e4a8296c5e66a0fd
richseungsu <a01045145179@gmail.com>
2025-12-08 22:37:16 +0900

    Momentum Update

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "572afd5c30427fc6bbb9f3d3180bba29";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  console.log("can't find you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
